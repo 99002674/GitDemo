fb92ac6e460fe270dff2086cb015371901ee42a5
99002674 <chandana.an@ltts.com>
2020-10-20 15:11:57 +0530

    Second Commit

--- GitDemo/FirstNetCoreApp/Program.cs
+++ GitDemo/FirstNetCoreApp/Program.cs
@@ -11,6 +11,10 @@ namespace FirstNetCoreApp
             MathOpDelegate delAdd = Add;
             int result = delAdd(55,32);
             Console.WriteLine("Result of Add:\t" + result.Tostring());
+            var arrInts = new int[4] {1,2,3,11}
+            foreach(var x in arrInts){
+                Console.WriteLine(x.Tostring());
+            }
         }
     }
 }
