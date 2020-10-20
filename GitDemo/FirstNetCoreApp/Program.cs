125e4200f24ed5de46ec8f536f5b25594901f5e0
99002674 <chandana.an@ltts.com>
2020-10-20 15:03:14 +0530

    First commit

---
+++ GitDemo/FirstNetCoreApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.Tostring());
+            MathOpDelegate delAdd = Add;
+            int result = delAdd(55,32);
+            Console.WriteLine("Result of Add:\t" + result.Tostring());
+        }
+    }
+}
